03eb49f2fb0bc2334bcf1bf36aec4baed377b0c1
kozue-y <sep_04_17_jaskoz@icloud.com>
2025-08-21 15:13:22 +0900

    スクロールバー固定まで

--- js/main.js
+++ js/main.js
@@ -4,3 +4,19 @@ $(".fv_slider").slick({
     arrows: false,
     pauseOnHover: false
 });
+
+// ヘッダー固定＆色変更
+$(function() {
+    const $header = $("#header");
+    const aboutTop = $("#about").offset().top;
+
+    $(window).on("scroll", function() {
+        const scroll = $(window).scrollTop();
+
+        if (scroll >= aboutTop) {
+            $header.addClass('scrolled');
+        } else {
+            $header.removeClass('scrolled');
+        }
+    });
+});
