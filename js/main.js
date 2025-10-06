714d1de024e4473eaf8fe70becf5fd4d5654b361
kozue-y <sep_04_17_jaskoz@icloud.com>
2025-10-07 01:52:18 +0900

    フッターSPまで

--- js/main.js
+++ js/main.js
@@ -83,6 +83,10 @@ $(function() {
       $("#page-top").removeClass("is-show");
     }
   });
+  $('#page-top').on('click', function() {
+    $('html, body').animate({ scrollTop: 0 } ,500);
+    return false;
+  });
   
   $("#page-top").on("click", function () {
     $("html, body").animate({ scrollTop: 0 }, 500);
